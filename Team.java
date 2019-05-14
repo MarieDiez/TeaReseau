8bfdcd29bcfde9a2c855fe9a590f64e736b018e3
Eterniitys <johan.guerrero394@gmail.com>
2019-05-14 18:57:08 +0000

    Team : update

--- Team.java
+++ Team.java
@@ -1,19 +1,31 @@
 import java.awt.Color;
 
 public enum Team {
-	noir(Color.black),
-	bleu(Color.blue),
-	rouge(Color.red),
-	vert(Color.green),
-	jaune(Color.yellow);
+	noir(0, Color.black),
+	bleu(1, Color.blue),
+	rouge(2, Color.red),
+	vert(3, Color.green),
+	jaune(4, Color.yellow);
 	
+	private int id;
 	private Color couleur;
 	
-	Team(Color col){
+	Team(int id, Color col){
+		this.id = id;
 		this.couleur = col;
 	}
 
 	public Color getCouleur() {
 		return couleur;
+	}
+	
+	public static Team getTeamById(int id) {
+		Team returnTeam = null;
+		for (Team t : Team.values()) {
+			if(t.id == id) {
+				returnTeam = t;
+			}
+		}
+		return returnTeam;
 	}
 }
